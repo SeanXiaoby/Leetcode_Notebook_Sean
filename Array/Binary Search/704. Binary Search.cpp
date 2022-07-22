ae6a0c2f8489596e7164da9527e2354cf79a552b
Boyang Xiao <sean_xiaoby@163.com>
2022-07-22 01:23:56 -0700

    Add Binary search folder

---
+++ Array/Binary Search/704. Binary Search.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+#include<vector>
+
+using namespace std;
+
+int search(vector<int>& nums, int target) {
+
+    
+
+    return 0;
+}
+
+
+int main(){
+    return 0;
+}
+
